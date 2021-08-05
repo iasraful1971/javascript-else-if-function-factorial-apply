33b7287c8cb86ad128010051b7391adccffda567
asraful <iasraful107@gmail.com>
2021-08-05 07:41:26 +0600

    push

---
+++ leap-year.js
@@ -0,0 +1,12 @@
+function leapYear (year){
+    if(year % 4 ==0){
+        return  'Leapyear';
+    }
+    else{
+        return 'no leap year';
+    }
+}
+const yearOne = leapYear(2021);
+console.log('the year is: ' ,yearOne);
+const yearTwo = leapYear(2124);
+console.log('the year is: ',yearTwo);
